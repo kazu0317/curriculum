9e1c49b339f9467c30d6343d52faa2c659d10ce1
カズ0317 <f.kazu0317shuukatu@gmail.com>
2024-11-15 11:51:50 +0000

    7-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,16 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
-
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        // getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
-}
 
+        /**
+         * 特定IDのpostを表示する
+         *
+         * @params Object Post // 引数の$postはid=1のPostインスタンス
+         * @return Reposnse post view
+         */
+        public function show(Post $post)
+        {
+            // $data = $post->id(1)->get();
+        
+            return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+        }
+}
